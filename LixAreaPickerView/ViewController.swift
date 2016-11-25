9b682b940dc2781eed87c27a44afa3d8fa8b4b25
Lix <xiao.liunit@gmail.com>
2016-11-25 08:04:05 +0800

    Initial Commit

---
+++ LixAreaPickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LixAreaPickerView
+//
+//  Created by Lix on 16/11/25.
+//  Copyright © 2016年 Lix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
